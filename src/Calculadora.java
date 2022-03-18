9cd27fb2ce26c4911a0b8b7abecf41379579dac4
Israel Jorge <israeljorge2000@gmail.com>
2022-03-18 10:05:08 -0300

    Initial commit

---
+++ src/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public int soma(int v1, int v2) {
+        return v1 + v2;
+    }
+    
+    public int sub( int v1, int v2) {
+        return v1 - v2;
+    }
+}
---
+++ src/Sistema.java
@@ -0,0 +1,95 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+
+public class Sistema extends JFrame 
+                                    implements ActionListener {
+    private static final long serialVersionUID = 1L;
+    
+    JLabel lValor1, lValor2, lRes;
+    JTextField tfValor1, tfValor2, tfRes;
+    JButton bSoma, bSub, bLimpar, bSair;
+    Calculadora c = new Calculadora();                                      
+
+    public Sistema() {
+        setLayout(new FlowLayout());
+        setSize(400, 500);
+        setVisible(true);
+        setTitle("Sistema");
+
+        lValor1 = new JLabel("Valor 1");
+        add(lValor1);
+        tfValor1 = new JTextField(5);
+        tfValor1.setToolTipText("Digite o Valor 1");
+        add(tfValor1);
+
+        lValor2 = new JLabel("Valor 2");
+        add(lValor2);
+        tfValor2 = new JTextField(5);
+        add(tfValor2);
+
+        lRes = new JLabel("Resposta");
+        add(lRes);
+        tfRes = new JTextField(5);
+        tfRes.setEditable(false); //Tira e edição do campo de texto.
+        add(tfRes);
+
+        bSoma = new JButton("Soma");
+        bSoma.addActionListener(this);
+        add(bSoma);
+        
+        bSub = new JButton("Subtrai");
+        bSub.addActionListener(this);
+        add(bSub);
+        
+        bLimpar = new JButton("Limpar");
+        bLimpar.addActionListener(this);
+        add(bLimpar);
+        
+        bSair = new JButton("Sair");
+        bSair.addActionListener(this);
+        add(bSair);
+    }
+
+
+
+    public static void main(final String[] args) throws Exception {
+        final Sistema s = new Sistema();
+        s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+        
+        if (ae.getSource() == bSoma) {
+            int valor1 = Integer.parseInt(tfValor1.getText()); // Transforma uma String em int (Em um valor);
+            int valor2 = Integer.parseInt(tfValor2.getText());
+            
+            int res = c.soma(valor1, valor2);            
+
+            tfRes.setText(String.valueOf(res)); //Transforma um  valor em em String;
+        }
+
+        if (ae.getSource() == bSub) {
+            int valor1 = Integer.parseInt(tfValor1.getText()); // Transforma uma String em int (Em um valor);
+            int valor2 = Integer.parseInt(tfValor2.getText());
+            
+            int res = c.sub(valor1, valor2);
+            
+            tfRes.setText(String.valueOf(res));
+        }
+       
+        if (ae.getSource() == bLimpar) {
+            tfValor1.setText(""); // Para limpar a caixa de texto
+            tfValor2.setText("");
+            tfRes.setText("");
+        }
+
+        if (ae.getSource() == bSair) {
+            System.exit(0);
+        }
+    }
+}
